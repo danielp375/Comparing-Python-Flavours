29e57e7fd85fd47c67e4df54252225e675318b4e
danielp375 <118190856+danielp375@users.noreply.github.com>
2023-07-27 22:13:21 +0530

    Update process_pandas.py

--- process_pandas.py
+++ process_pandas.py
@@ -1,16 +1,14 @@
+#Module used to get the time in seconds
 import time 
-
 start_time = time.time()
-
+#Module used for data processing
 import pandas as pd
-
- 
+#importing the dataset
 data = pd.read_csv("dataset.csv")
-
+#displaying the details, first few columns and correlation
 print(data.info())
 print(data.head())
 print(data.corr())
-
 print("\n")
-
+#displaying the time taken for the program to run
 print(time.time() - start_time,"seconds to run program on pypy.")
